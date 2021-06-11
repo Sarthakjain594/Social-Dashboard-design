1c5219165e20c3b3362544014cf76a7c2d8b5cab
SarthakJain594 <jsarthak448@gmail.com>
2021-06-11 15:17:59 +0530

    Mobile design complete

---
+++ app.js
@@ -0,0 +1,20 @@
+let themebtn = document.querySelector(".checkbox"); 
+let themelabel = document.querySelector(".switch-toggle");
+let box2sec = document.querySelector(".themetoggle")
+themebtn.addEventListener("change" ,() =>{
+    document.body.classList.toggle("light");
+    document.querySelector(".sec1").classList.toggle("light");
+    document.querySelector(".sec2").classList.toggle("light");
+    document.querySelector(".sec3").classList.toggle("light");
+    document.querySelector(".sec4").classList.toggle("light");
+    document.querySelector(".sec21").classList.toggle("light");
+    document.querySelector(".sec22").classList.toggle("light");
+    document.querySelector(".sec23").classList.toggle("light");
+    document.querySelector(".sec24").classList.toggle("light");
+    document.querySelector(".sec25").classList.toggle("light");
+    document.querySelector(".sec26").classList.toggle("light");
+    document.querySelector(".sec27").classList.toggle("light");
+    document.querySelector(".sec28").classList.toggle("light");
+    themelabel.classList.toggle("light");
+    
+})
